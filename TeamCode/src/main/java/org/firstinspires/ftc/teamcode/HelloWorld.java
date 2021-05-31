e795d25802246d207999ee0c93ef46efe49f5c75
saanson84 <saanson84@gmail.com>
2021-05-31 11:16:47 -0500

    Adding a HelloWorld example program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -5,11 +5,15 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp()
 public class HelloWorld extends OpMode {
+
+// This is called when the driver presses INIT
+
     @Override
     public void init() {
         telemetry.addData("Hello","World");
     }
 
+// This is called repeatedly while OpMode is playing
     @Override
     public void loop() {
 
