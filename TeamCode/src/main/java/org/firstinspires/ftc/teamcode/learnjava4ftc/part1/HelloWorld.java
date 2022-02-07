b99d753d05cb1e880fa6d269bd12d835130c7754
Scott A <60680479+saanson84@users.noreply.github.com>
2022-02-06 18:46:40 -0600

    Update HelloWorld.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learnjava4ftc/part1/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/learnjava4ftc/part1/HelloWorld.java
@@ -5,11 +5,18 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp()
 public class HelloWorld extends OpMode {
+  
+  /*
+  *   This method is called when the OpMode is initialized
+  */
   @Override
   public void init() {
     telemetry.addData("Hello", "World");
   }
   
+  /*
+  *   This method does nothing while the OpMode is running.
+  */
   @Override
   public void loop {
     
